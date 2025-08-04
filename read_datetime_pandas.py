79a72fa2f4273362b40f08b10d6f7cdbb07b5c85
dangerous97-commits <ektasinhaunity@gmail.com>
2025-08-04 21:57:34 +0530

    Update read_datetime_pandas.py

--- read_datetime_pandas.py
+++ read_datetime_pandas.py
@@ -16,6 +16,7 @@ df['dt_customer'] = pd.to_datetime(df['dt_customer'],format='%d-%m-%Y')
 df.to_csv(r'C:\Users\ektas\OneDrive\Documents\marketing_kaggle_clean.csv')
 print(df.dtypes)
 
-# Check and fix data types (e.g., age should be int, date as datetime).
+# Check and fix data types (e.g.,date as datetime).
 df['dt_customer'] = pd.to_datetime(df['dt_customer'],format='%d-%m-%Y')
 print(df.dtypes)
+
